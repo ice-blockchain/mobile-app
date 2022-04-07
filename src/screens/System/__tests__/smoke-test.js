fc0bfb870ca1b2904ff69870dad546647ca2b8e2
Oleg Panfyorov <oleg.panforov@framey.io>
2022-04-07 18:39:32 +0300

    Added husky

--- src/screens/System/__tests__/smoke-test.js
+++
@@ -1,8 +0,0 @@
-import 'react-native';
-import React from 'react';
-import Component from '../index';
-import renderer from 'react-test-renderer';
-
-it(`${Component.name} renders correctly`, () => {
-  renderer.create(<Component />);
-});
