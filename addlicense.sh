25b11eeedaf9a0177d0da0656590f004b78fc4be
Oleg Panfyorov <oleg.panforov@framey.io>
2022-04-08 17:51:54 +0300

    Fixed script

--- addlicense.sh
+++ addlicense.sh
@@ -1,1 +1,13 @@
-for x in $*; do head -n 1 $x | diff LICENSE.header - || ( ( cat LICENSE.header; echo; cat $x) > /tmp/file; mv /tmp/file $x ) done
+#!/bin/bash
+declare -i files_count=0
+
+for x in $*; 
+do head -n 1 $x | diff LICENSE.header - || ( ( cat LICENSE.header; echo; cat $x) > /tmp/file; mv /tmp/file $x;
+(( files_count++ ));  ) 
+done
+echo "Total counter: $files_count"
+if [ $files_count -gt 0 ]
+then
+echo "There were $files_count files without license"
+exit 1
+fi
