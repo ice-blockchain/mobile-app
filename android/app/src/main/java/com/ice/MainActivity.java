9c4232e635369a1285e571224287ba16da83ed5f
Oleg Panfyorov <oleg.panforov@framey.io>
2022-04-13 16:49:51 +0300

    Added android splash layout without assets

--- android/app/src/main/java/com/ice/MainActivity.java
+++ android/app/src/main/java/com/ice/MainActivity.java
@@ -3,6 +3,7 @@ package com.ice;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
