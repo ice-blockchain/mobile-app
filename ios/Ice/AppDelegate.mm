c55921d19e8fd143861c7bb84d52053ac98f1601
nikita.subbotin <nikita.subbotin@framey.io>
2022-05-23 16:23:33 +0300

    ios setup

--- ios/Ice/AppDelegate.mm
+++ ios/Ice/AppDelegate.mm
@@ -5,6 +5,7 @@
 #import <React/RCTRootView.h>
 
 #import <React/RCTAppSetupUtils.h>
+#import <Firebase.h>
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
@@ -54,6 +55,8 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
+
   return YES;
 }
 
