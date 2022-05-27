d9d437e4d79c5dbd3f4877a5aa35f0ece16a15be
subbotnik16 <nikita.subbotin@framey.io>
2022-05-27 16:24:23 +0300

    Renamed svg icons to PascalCase

---
+++ src/assets/svg/CloseIcon.tsx
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+import * as React from 'react';
+import {Svg, Path} from 'react-native-svg';
+
+export const CloseIconSvg = () => {
+  return (
+    <Svg width="9" height="10" viewBox="0 0 9 10" fill="none">
+      <Path
+        d="M8.07279 1.04649L0.99999 8.25459"
+        stroke="#0C56C5"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+      <Path
+        d="M0.999998 1.04649L8.07279 8.25459"
+        stroke="#0C56C5"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </Svg>
+  );
+};
---
+++ src/assets/svg/NextArrow.tsx
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+import * as React from 'react';
+import {Svg, Rect} from 'react-native-svg';
+
+export const NextArrowSvg = () => {
+  return (
+    <Svg width="12" height="11" viewBox="0 0 12 11" fill="none">
+      <Rect y="5" width="11" height="1.5" rx="0.75" fill="white" />
+      <Rect
+        x="7.19995"
+        y="0.600098"
+        width="6.70096"
+        height="1.5"
+        rx="0.75"
+        transform="rotate(52.0858 7.19995 0.600098)"
+        fill="white"
+      />
+      <Rect
+        width="6.70096"
+        height="1.5"
+        rx="0.75"
+        transform="matrix(0.614481 -0.788932 -0.788932 -0.614481 7.19995 10.8999)"
+        fill="white"
+      />
+    </Svg>
+  );
+};
