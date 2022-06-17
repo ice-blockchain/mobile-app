c986bda96bd31a432008b525f62dc9f1b1d05ea2
pisarevkir <kirill.pisarev@framey.io>
2022-06-17 15:51:40 +0300

    chore: make only avatar pen pressable

--- src/screens/Profile/components/UserInfo/components/Avatar.tsx
+++ src/screens/Profile/components/UserInfo/components/Avatar.tsx
@@ -3,7 +3,7 @@
 import {COLORS} from '@constants/colors';
 import {PenIcon} from '@svg/PenIcon';
 import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
 import {rem} from 'rn-units';
 
 const AVATAR_SIZE = rem(112);
@@ -12,18 +12,21 @@ const PEN_SIZE = rem(26);
 type Props = {
   uri: string;
   showPen: boolean;
+  onPenPress?: () => void;
 };
 
-export const Avatar = ({uri, showPen}: Props) => {
+export const Avatar = ({uri, showPen, onPenPress}: Props) => {
   return (
     <View>
       <View style={styles.avatarWrapper}>
         <Image source={{uri}} style={styles.image} />
       </View>
       {showPen && (
-        <View style={styles.penWrapper}>
-          <PenIcon width={rem(14)} height={rem(14)} style={styles.pen} />
-        </View>
+        <TouchableOpacity style={styles.button} onPress={onPenPress}>
+          <View style={styles.penWrapper}>
+            <PenIcon width={rem(14)} height={rem(14)} style={styles.pen} />
+          </View>
+        </TouchableOpacity>
       )}
     </View>
   );
@@ -40,16 +43,20 @@ const styles = StyleSheet.create({
     width: AVATAR_SIZE,
     height: AVATAR_SIZE,
   },
-  penWrapper: {
+  button: {
     position: 'absolute',
-    bottom: -1,
-    right: -1,
+    bottom: -10,
+    right: -10,
+  },
+  penWrapper: {
     alignItems: 'center',
     justifyContent: 'center',
     width: PEN_SIZE,
     height: PEN_SIZE,
     borderRadius: PEN_SIZE / 2,
     backgroundColor: COLORS.white,
+    marginHorizontal: 10,
+    marginVertical: 10,
   },
   pen: {
     marginBottom: 1,
--- src/screens/Profile/components/UserInfo/index.tsx
+++ src/screens/Profile/components/UserInfo/index.tsx
@@ -7,23 +7,21 @@ import {Avatar} from '@screens/Profile/components/UserInfo/components/Avatar';
 import {Level} from '@screens/Profile/components/UserInfo/components/Level';
 import {formatNumber} from '@utils/number';
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {font, rem} from 'rn-units';
 
 export const UserInfo = () => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
-        <View style={styles.body}>
-          <Avatar
-            showPen
-            uri="https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo="
-          />
-          <Text style={styles.usernameText} numberOfLines={1}>
-            @johnyknox07
-          </Text>
-        </View>
-      </TouchableOpacity>
+      <View style={styles.body}>
+        <Avatar
+          showPen
+          uri="https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo="
+        />
+        <Text style={styles.usernameText} numberOfLines={1}>
+          @johnyknox07
+        </Text>
+      </View>
       <View style={styles.ladder}>
         <View style={styles.ladderLeft}>
           <Text style={styles.ladderValueText}>{formatNumber(606683)}</Text>
