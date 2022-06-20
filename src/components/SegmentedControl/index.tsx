f53aa68055ec0a336416909e6eda259e04555eaf
pisarevkir <kirill.pisarev@framey.io>
2022-06-20 10:06:11 +0300

    feat: add SegmentedControl

---
+++ src/components/SegmentedControl/index.tsx
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+import {COLORS} from '@constants/colors';
+import {FONTS} from '@constants/fonts';
+import {commonStyles} from '@constants/styles';
+import {t} from '@utils/i18n';
+import React, {useMemo, useRef, useState} from 'react';
+import {
+  Animated,
+  FlexStyle,
+  LayoutChangeEvent,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native';
+import {font} from 'rn-units';
+
+const DEFAULT_MARGIN = 8;
+const CONTROL_HEIGHT = 55;
+
+type Segment = {text: string; key: string};
+
+type Props = {
+  segments: ReadonlyArray<Segment>;
+  onPress?: (index: number) => void;
+  style?: StyleProp<ViewStyle | FlexStyle>;
+};
+
+export const SegmentedControl = ({
+  segments = [],
+  style,
+  onPress = () => {},
+}: Props) => {
+  const controlWidth = useRef(0);
+  const [translateValue] = useState(new Animated.Value(0));
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const dynamicStyle = useMemo(
+    () =>
+      StyleSheet.create({
+        indicator: {
+          width: 100 / segments.length + '%',
+        },
+      }),
+    [segments.length],
+  );
+
+  const onSegmentSelect = async (index: number) => {
+    Animated.spring(translateValue, {
+      toValue: index * (controlWidth.current / segments.length),
+      velocity: 10,
+      useNativeDriver: true,
+    }).start();
+    setActiveIndex(index);
+  };
+
+  const onLayout = (event: LayoutChangeEvent) => {
+    const {width} = event.nativeEvent.layout;
+    controlWidth.current = width;
+  };
+
+  const renderSegment = (segment: Segment, index: number) => {
+    const isActive = activeIndex === index;
+    return (
+      <TouchableOpacity
+        key={segment.key}
+        onPress={() => {
+          onSegmentSelect(index);
+          onPress(index);
+        }}
+        style={styles.segment}>
+        <Text
+          style={[
+            styles.text,
+            isActive ? styles.activeText : styles.inactiveText,
+          ]}>
+          {t(segment.text)}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={[styles.container, commonStyles.shadow, style]}>
+      <View style={styles.body} onLayout={onLayout}>
+        <Animated.View
+          style={[
+            dynamicStyle.indicator,
+            styles.indicator,
+            StyleSheet.absoluteFill,
+            {transform: [{translateX: translateValue}]},
+          ]}
+        />
+        {segments.map(renderSegment)}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 16,
+    backgroundColor: COLORS.white,
+    paddingHorizontal: DEFAULT_MARGIN,
+    height: CONTROL_HEIGHT,
+  },
+  body: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+  indicator: {
+    backgroundColor: COLORS.primary,
+    borderRadius: 12,
+    marginVertical: DEFAULT_MARGIN,
+  },
+  segment: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: font(16),
+    fontFamily: FONTS.primary.bold,
+  },
+  activeText: {
+    color: COLORS.white,
+  },
+  inactiveText: {
+    color: COLORS.darkBlue,
+  },
+});
