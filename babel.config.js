2515996ea01070bd06b90171d9040141c5b95518
pisarevkir <kirill.pisarev@framey.io>
2022-06-23 14:31:30 +0300

    chore: cleanup tsconfig, bacelrc

--- babel.config.js
+++ babel.config.js
@@ -27,8 +27,6 @@ module.exports = api => {
             '@translations': './src/translations',
             '@utils': './src/utils',
             '@hooks': './src/hooks',
-            'mock-data': './test/mock-data',
-            test: './test',
           },
         },
       ],
