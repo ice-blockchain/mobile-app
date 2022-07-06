19fa2ad14dd5dc8d3a0ac855f2117848a3142c86
pisarevkir <kirill.pisarev@framey.io>
2022-07-06 10:59:09 +0300

    feat: add Switch component

---
+++ src/components/Switch/index.tsx
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+import {COLORS} from '@constants/colors';
+import {SMALL_BUTTON_HIT_SLOP} from '@constants/styles';
+import React, {useEffect} from 'react';
+import {StyleSheet, TouchableWithoutFeedback} from 'react-native';
+import Animated, {
+  interpolate,
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {rem} from 'rn-units';
+
+export type SwitchProps = {
+  value: boolean;
+  onValueChange: (value: boolean) => void;
+};
+
+const CONTAINER_WIDTH = rem(18);
+const CONTAINER_HEIGHT = rem(12);
+const CONTAINER_BORDER_RADIUS = rem(7);
+const CONTAINER_BORDER_WIDTH = 1;
+const BALL_SIZE = rem(8);
+const BALL_SIDE_OFFSET = 1;
+
+export const Switch = ({value, onValueChange}: SwitchProps) => {
+  const stateSharedValue = useSharedValue(0);
+
+  const animatedBallStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: interpolate(
+            stateSharedValue.value,
+            [0, 1],
+            [
+              BALL_SIDE_OFFSET,
+              CONTAINER_WIDTH -
+                BALL_SIZE -
+                BALL_SIDE_OFFSET -
+                CONTAINER_BORDER_WIDTH * 2,
+            ],
+          ),
+        },
+      ],
+      backgroundColor: interpolateColor(
+        stateSharedValue.value,
+        [0, 1],
+        [COLORS.white, COLORS.darkBlue],
+      ),
+    };
+  });
+
+  const animatedContainerStyles = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        stateSharedValue.value,
+        [0, 1],
+        [COLORS.darkBlue, COLORS.white],
+      ),
+    };
+  });
+
+  useEffect(() => {
+    stateSharedValue.value = withTiming(value ? 1 : 0);
+  }, [stateSharedValue, value]);
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => onValueChange(!value)}
+      hitSlop={SMALL_BUTTON_HIT_SLOP}>
+      <Animated.View style={[styles.container, animatedContainerStyles]}>
+        <Animated.View style={[styles.ball, animatedBallStyles]} />
+      </Animated.View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: CONTAINER_WIDTH,
+    height: CONTAINER_HEIGHT,
+    borderWidth: CONTAINER_BORDER_WIDTH,
+    borderColor: COLORS.darkBlue,
+    borderRadius: CONTAINER_BORDER_RADIUS,
+    justifyContent: 'center',
+  },
+  ball: {
+    width: BALL_SIZE,
+    height: BALL_SIZE,
+    borderRadius: BALL_SIZE / 2,
+  },
+});
--- src/screens/SettingsFlow/Notifications/index.tsx
+++ src/screens/SettingsFlow/Notifications/index.tsx
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: BUSL-1.1
 
 import {Avatar} from '@components/Avatar';
+import {Switch} from '@components/Switch';
 import {COLORS} from '@constants/colors';
 import {commonStyles} from '@constants/styles';
 import {Header} from '@navigation/components/Header';
 import {LangButton} from '@navigation/components/Header/components/LangButton';
 import {useBottomTabBarOffsetStyle} from '@navigation/hooks/useBottomTabBarOffsetStyle';
 import {useFocusStatusBar} from '@navigation/hooks/useFocusStatusBar';
-import React, {memo} from 'react';
+import React, {memo, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {rem} from 'rn-units';
 
 export const Notifications = memo(() => {
   useFocusStatusBar({style: 'light-content'});
   const bottomOffset = useBottomTabBarOffsetStyle();
+  const [value, setValue] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -30,7 +32,9 @@ export const Notifications = memo(() => {
           uri="https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo="
           style={styles.avatar}
         />
-        <View style={styles.body} />
+        <View style={styles.body}>
+          <Switch value={value} onValueChange={setValue} />
+        </View>
       </View>
     </View>
   );
