00497bc17c36264d568c9b4d0b26595bd7534bb3
pisarevkir <kirill.pisarev@framey.io>
2022-07-07 11:36:27 +0300

    chore: remove linking stub

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -11,7 +11,6 @@ import {
   userDataSelector,
 } from '@store/modules/Auth/selectors';
 import React, {useEffect} from 'react';
-import {Linking} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 
 import {AuthNavigator} from './Auth';
@@ -38,19 +37,9 @@ function ActiveNavigator() {
 }
 
 export function Router() {
-  const initializeDynamicLinks = async () => {
-    Linking.addEventListener('url', ({url}) => console.log(url));
-    Linking.getInitialURL().then(url => {
-      if (url) {
-        console.log(url);
-      }
-    });
-  };
-
   useAppStateListener();
-
   return (
-    <NavigationContainer onReady={initializeDynamicLinks} theme={theme}>
+    <NavigationContainer theme={theme}>
       <ActiveNavigator />
     </NavigationContainer>
   );
