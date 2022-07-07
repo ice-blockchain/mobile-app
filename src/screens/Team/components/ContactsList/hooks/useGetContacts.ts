9669750daf1ceba465d56d33e23a65b1471fc5c9
subbotnik16 <nikita.subbotin@framey.io>
2022-07-07 18:22:09 +0300

    chore: fix behaviour for invited contacts

--- src/screens/Team/components/ContactsList/hooks/useGetContacts.ts
+++ src/screens/Team/components/ContactsList/hooks/useGetContacts.ts
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: BUSL-1.1
 
 import {TeamActions} from '@store/modules/Team/actions';
-import {
-  getContactsIdsSelector,
-  getIceFriendsSelector,
-} from '@store/modules/Team/selectors';
+import {getContactsIdsSelector} from '@store/modules/Team/selectors';
 import {t} from '@translations/i18n';
 import {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
 export const useGetContacts = () => {
-  const iceFriends = useSelector(getIceFriendsSelector);
   const contactsIds = useSelector(getContactsIdsSelector);
   const dispatch = useDispatch();
 
@@ -22,22 +18,14 @@ export const useGetContacts = () => {
 
   const sections = [
     {
+      title: 'iceFriends',
+      data: ['InviteFriendsButton'],
+    },
+    {
       title: t('team.contacts_list.all_contacts'),
       data: contactsIds,
     },
   ];
-
-  if (iceFriends.length) {
-    sections.unshift({
-      title: 'iceFriends',
-      data: iceFriends,
-    });
-  } else {
-    sections.unshift({
-      title: 'iceFriends',
-      data: ['InviteFriendsButton'],
-    });
-  }
 
   return sections;
 };
