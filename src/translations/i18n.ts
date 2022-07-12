8d52991abcc22d98d08f9662a5308c9de086d8ab
pisarevkir <kirill.pisarev@framey.io>
2022-07-12 12:30:06 +0300

    chore: remove missingTranslation from i18n

--- src/translations/i18n.ts
+++ src/translations/i18n.ts
@@ -14,7 +14,6 @@ i18n.defaultLocale = 'en-GB';
 i18n.locale = locale?.languageTag || i18n.defaultLocale;
 i18n.fallbacks = true;
 i18n.translations = locales;
-i18n.missingTranslation = (scope: string) => scope;
 
 export default i18n;
 
