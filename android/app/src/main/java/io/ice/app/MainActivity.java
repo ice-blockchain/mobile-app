e3a0757a74853255bf836dea246daef672f12f5a
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-03-27 13:03:00 +0800

    build(android): fix MainActivity deps

--- android/app/src/main/java/io/ice/app/MainActivity.java
+++ android/app/src/main/java/io/ice/app/MainActivity.java
@@ -2,6 +2,7 @@
 
 package io.ice.app;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -24,7 +25,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(null);
+    super.onCreate(null);
   }
 
   /**
