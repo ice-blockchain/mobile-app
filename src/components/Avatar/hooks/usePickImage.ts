a0e7bce567ac492c93beb6dd8c6ed0c42e85ce79
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-03-31 12:03:00 +0800

    fix: android image picker for webp

--- src/components/Avatar/hooks/usePickImage.ts
+++ src/components/Avatar/hooks/usePickImage.ts
@@ -2,6 +2,7 @@
 
 import {FULL_SCREEN_IMAGE_SIZE} from '@constants/images';
 import {logError} from '@services/logging';
+import {checkProp} from '@utils/guards';
 import ImagePicker, {
   Image,
   Options,
@@ -15,22 +16,40 @@ export const usePickImage = ({
   onImageSelected: (image: Image) => void;
   options?: Options;
 }) => {
-  const openPicker = (mode: 'gallery' | 'camera') =>
-    ImagePicker[mode === 'gallery' ? 'openPicker' : 'openCamera']({
-      width: FULL_SCREEN_IMAGE_SIZE,
-      height: FULL_SCREEN_IMAGE_SIZE,
-      mediaType: 'photo',
-      cropping: true,
-      forceJpg: true,
-      includeBase64: false,
-      includeExif: false,
-      ...options,
-    })
-      .then(onImageSelected)
-      .catch(error => {
-        if ((error.code as PickerErrorCode) !== 'E_PICKER_CANCELLED') {
-          logError(error);
-        }
+  const openPicker = async (mode: 'gallery' | 'camera') => {
+    try {
+      const selectedImage = await ImagePicker[
+        mode === 'gallery' ? 'openPicker' : 'openCamera'
+      ]({
+        mediaType: 'photo',
+        cropping: false,
+        forceJpg: true,
+        includeBase64: false,
+        includeExif: false,
+        ...options,
       });
+      /**
+       * Call openPicker/openCamera and openCropper separately
+       * so library converts selected image to jpg before cropping.
+       * This workaround saves from variety of errors on android (e.g. when picking webp)
+       */
+      const croppedImage = await ImagePicker.openCropper({
+        mediaType: 'photo',
+        path: selectedImage.path,
+        width: FULL_SCREEN_IMAGE_SIZE,
+        height: FULL_SCREEN_IMAGE_SIZE,
+        includeBase64: false,
+        includeExif: false,
+      });
+      onImageSelected(croppedImage);
+    } catch (error) {
+      if (
+        checkProp(error, 'code') &&
+        (error.code as PickerErrorCode) !== 'E_PICKER_CANCELLED'
+      ) {
+        logError(error);
+      }
+    }
+  };
   return {openPicker, onImageSelected};
 };
