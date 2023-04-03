e2f5e1b751f97011f95f5795503d155c1979b9eb
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-04-03 12:46:34 +0800

    feat: add feature flags constants

---
+++ src/constants/featureFlags.ts
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import {deviceMainLocale} from '@translations/i18n';
+
+/**
+ * Is Email Link sign-in flow used instead of Email + Password
+ */
+export const isEmailLinkSignIn = deviceMainLocale.languageCode !== 'zh';
+
+/**
+ * Is enabled:
+ * 	- Sign-in with phone number
+ * 	- Link phone number on the Team Screen
+ *  - Change phone number on the Settings Screen
+ */
+export const isPhoneNumberEnabled = deviceMainLocale.languageCode !== 'zh';
