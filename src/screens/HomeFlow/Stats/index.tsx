3807fd397474a0f3b270204baa1c15c95eee4e96
ice-atlas <121009572+ice-atlas@users.noreply.github.com>
2023-04-06 19:05:05 +0300

    feat(pull-to-refresh): implement new pull-to-refresh container

--- src/screens/HomeFlow/Stats/index.tsx
+++ src/screens/HomeFlow/Stats/index.tsx
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import {LinesBackground} from '@components/LinesBackground';
-import {RefreshControl} from '@components/RefreshControl';
+import {PullToRefreshContainer} from '@components/PullToRefreshContainer';
 import {COLORS} from '@constants/colors';
 import {commonStyles} from '@constants/styles';
 import {Header} from '@navigation/components/Header';
@@ -14,10 +14,7 @@ import {UsersGrowthGraph} from '@screens/HomeFlow/Stats/components/UsersGrowthGr
 import {t} from '@translations/i18n';
 import React, {memo} from 'react';
 import {StyleSheet, View} from 'react-native';
-import Animated, {
-  useAnimatedScrollHandler,
-  useSharedValue,
-} from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
 
 import {useOnRefresh} from './hooks/useOnRefresh';
 
@@ -26,42 +23,38 @@ export const Stats = memo(() => {
 
   const tabbarOffset = useBottomTabBarOffsetStyle();
 
-  const translateY = useSharedValue(0);
-
-  const scrollHandler = useAnimatedScrollHandler(({contentOffset: {y}}) => {
-    translateY.value = y;
-  });
-
   const {refreshing, onRefresh} = useOnRefresh();
 
   return (
     <View style={styles.container}>
       <LinesBackground />
+
       <Header
         color={COLORS.white}
         title={t('stats.header_title')}
         backgroundColor={'transparent'}
       />
+
       <Summary />
-      <Animated.ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={tabbarOffset.current}
-        scrollEventThrottle={16}
-        onScroll={scrollHandler}
-        refreshControl={
-          <RefreshControl
-            theme={'dark-content'}
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-            translateY={translateY}
-          />
-        }>
-        <View style={[styles.card, commonStyles.baseSubScreen]}>
+
+      <PullToRefreshContainer
+        style={commonStyles.flexOne}
+        theme={'dark-content'}
+        refreshing={refreshing}
+        onRefresh={onRefresh}>
+        <Animated.ScrollView
+          contentContainerStyle={[
+            commonStyles.baseSubScreen,
+            tabbarOffset.current,
+          ]}
+          showsVerticalScrollIndicator={false}>
           <UsersGrowthGraph />
+
           <TopMiners />
+
           <TopCountries />
-        </View>
-      </Animated.ScrollView>
+        </Animated.ScrollView>
+      </PullToRefreshContainer>
     </View>
   );
 });
@@ -70,9 +63,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.primaryLight,
-  },
-  card: {
-    paddingBottom: 2000,
-    marginBottom: -2000,
   },
 });
