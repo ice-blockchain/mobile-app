26f202c87d54637a04150b3c4121238d37617531
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2023-04-20 16:07:21 +0300

    chore: removed extra space

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import {achievements} from '@api/achievements';
+import {devices} from '@api/devices';
+import {news} from '@api/news';
+import {notifications} from '@api/notifications';
+import {referrals} from '@api/referrals';
+import {statistics} from '@api/statistics';
 import {statusNotice} from '@api/statusNotice';
-
-import {achievements} from './achievements';
-import {devices} from './devices';
-import {news} from './news';
-import {notifications} from './notifications';
-import {referrals} from './referrals';
-import {statistics} from './statistics';
-import {tasks} from './tasks';
-import {time} from './time';
-import {tokenomics} from './tokenomics';
-import {user} from './user';
-import {validations} from './validations';
+import {tasks} from '@api/tasks';
+import {time} from '@api/time';
+import {tokenomics} from '@api/tokenomics';
+import {user} from '@api/user';
+import {validations} from '@api/validations';
 
 export const Api = Object.freeze({
   achievements,
