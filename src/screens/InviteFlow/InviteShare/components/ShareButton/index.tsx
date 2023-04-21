2d50a800b4c5cf51fbc2b538542039c25e9c941c
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-04-21 12:33:26 +0400

    refactor: share card

--- src/screens/InviteFlow/InviteShare/components/ShareButton/index.tsx
+++ src/screens/InviteFlow/InviteShare/components/ShareButton/index.tsx
@@ -1,44 +1,28 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import {Touchable} from '@components/Touchable';
-import {SocialType} from '@screens/InviteFlow/InviteShare/components/ShareButton/types';
 import {font} from '@utils/styles';
 import React from 'react';
-import {Image, StyleSheet, Text} from 'react-native';
-import {Social} from 'react-native-share';
+import {Image, StyleProp, StyleSheet, Text, ViewStyle} from 'react-native';
 import {rem} from 'rn-units';
 
-export type SocialShareButtonType = {
-  type: SocialType;
+type Props = {
+  onPress: () => void;
   title: string;
   icon: number;
-  social?: Social;
+  style?: StyleProp<ViewStyle>;
 };
 
-interface ShareButtonProps {
-  button: SocialShareButtonType;
-  onPress: (type: SocialType) => void;
-}
-export const ShareButton = ({button, onPress}: ShareButtonProps) => {
-  const handlePress = () => {
-    onPress(button.type);
-  };
-
+export const ShareButton = ({title, icon, onPress, style}: Props) => {
   return (
-    <Touchable style={styles.button} onPress={handlePress}>
-      <Image style={styles.icon} source={button.icon} />
-      <Text style={styles.buttonTitle}>{button.title}</Text>
+    <Touchable style={style} onPress={onPress}>
+      <Image style={styles.icon} source={icon} />
+      <Text style={styles.buttonTitle}>{title}</Text>
     </Touchable>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
-    width: '25%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: rem(35),
-  },
   icon: {
     width: rem(56),
     height: rem(56),
--- src/screens/InviteFlow/InviteShare/components/ShareCard/index.tsx
+++ src/screens/InviteFlow/InviteShare/components/ShareCard/index.tsx
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import {COLORS} from '@constants/colors';
 import {LINKS} from '@constants/links';
 import {commonStyles} from '@constants/styles';
-import {useSafeAreaInsets} from '@hooks/useSafeAreaInsets';
+import {Images} from '@images';
+import {useBottomOffsetStyle} from '@navigation/hooks/useBottomOffsetStyle';
 import Clipboard from '@react-native-clipboard/clipboard';
 import {
   Copied,
   CopiedMethods,
 } from '@screens/InviteFlow/InviteShare/components/Copied';
-import {
-  ShareButton,
-  SocialShareButtonType,
-} from '@screens/InviteFlow/InviteShare/components/ShareButton';
-import {SocialType} from '@screens/InviteFlow/InviteShare/components/ShareButton/types';
+import {ShareButton} from '@screens/InviteFlow/InviteShare/components/ShareButton';
 import {logError} from '@services/logging';
 import {usernameSelector} from '@store/modules/Account/selectors';
 import {AnalyticsEventLogger} from '@store/modules/Analytics/constants';
@@ -31,65 +29,58 @@ import Share, {ShareSingleOptions, Social} from 'react-native-share';
 import {useSelector} from 'react-redux';
 import {isAndroid, isIOS, rem} from 'rn-units';
 
-const telegramIcon = require('../../assets/images/telegramIcon.png');
-const twitterIcon = require('../../assets/images/twitterIcon.png');
-const whatsAppIcon = require('../../assets/images/whatsAppIcon.png');
-const emailIcon = require('../../assets/images/emailIcon.png');
-const fbIcon = require('../../assets/images/newsfeedIcon.png');
-const copyIcon = require('../../assets/images/linkIcon.png');
-const moreIcon = require('../../assets/images/moreIcon.png');
-const smsIcon = require('../../assets/images/smsIcon.png');
-
-const buttons: SocialShareButtonType[] = [
+const buttons = [
   {
     type: 'WhatsApp',
     title: t('invite_share.whatsapp'),
-    icon: whatsAppIcon,
+    icon: Images.share.whatsApp,
   },
   {
     type: 'Telegram',
     title: t('invite_share.telegram'),
-    icon: telegramIcon,
+    icon: Images.share.telegram,
   },
   {
     type: 'Twitter',
     title: t('invite_share.twitter'),
-    icon: twitterIcon,
+    icon: Images.share.twitter,
   },
   {
     type: 'FB',
     title: t('invite_share.fb'),
-    icon: fbIcon,
+    icon: Images.share.facebook,
     social: Share.Social.FACEBOOK,
   },
   {
     type: 'Email',
     title: t('invite_share.email'),
-    icon: emailIcon,
+    icon: Images.share.email,
   },
   {
     type: 'Sms',
     title: t('invite_share.sms'),
-    icon: smsIcon,
+    icon: Images.share.sms,
   },
   {
     type: 'CopyLink',
     title: t('invite_share.copy_link'),
-    icon: copyIcon,
+    icon: Images.share.link,
   },
   {
     type: 'More',
     title: t('invite_share.more'),
-    icon: moreIcon,
+    icon: Images.share.more,
   },
-];
+] as const;
 
 export const ShareCard = () => {
   const copiedRef = useRef<CopiedMethods>(null);
-  const {bottom: bottomInset} = useSafeAreaInsets();
+  const bottomOffset = useBottomOffsetStyle();
   const username = useSelector(usernameSelector);
 
-  const handleSocialButtonPress = async (type: SocialType) => {
+  const handleSocialButtonPress = async (
+    type: typeof buttons[number]['type'],
+  ) => {
     const url = `${LINKS.MAIN}@${username}`;
     const baseOptions = {message: t('invite_share.share_message'), url};
     AnalyticsEventLogger.trackInvite({inviteAppType: type});
@@ -165,23 +156,23 @@ export const ShareCard = () => {
   };
 
   return (
-    <View style={[styles.fullCard, {height: rem(283) + bottomInset}]}>
+    <View
+      style={[
+        styles.shareCard,
+        commonStyles.baseSubScreen,
+        bottomOffset.current,
+      ]}>
       <Copied ref={copiedRef} />
-      <View
-        style={[
-          styles.shareCard,
-          commonStyles.baseSubScreen,
-          {height: rem(239) + bottomInset},
-        ]}>
-        <View style={styles.buttonsContainer}>
-          {buttons.map(button => (
-            <ShareButton
-              button={button}
-              key={button.type}
-              onPress={handleSocialButtonPress}
-            />
-          ))}
-        </View>
+      <View style={styles.buttonsContainer}>
+        {buttons.map(button => (
+          <ShareButton
+            title={button.title}
+            icon={button.icon}
+            key={button.type}
+            onPress={() => handleSocialButtonPress(button.type)}
+            style={styles.button}
+          />
+        ))}
       </View>
     </View>
   );
@@ -206,12 +197,6 @@ const isShareProviderNotInstalled = (error: unknown) => {
 };
 
 const styles = StyleSheet.create({
-  fullCard: {
-    position: 'absolute',
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
   shareCard: {
     position: 'absolute',
     left: 0,
@@ -222,5 +207,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginHorizontal: rem(12),
+    marginVertical: rem(12),
+    backgroundColor: COLORS.white,
+  },
+  button: {
+    width: '25%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: rem(24),
   },
 });
