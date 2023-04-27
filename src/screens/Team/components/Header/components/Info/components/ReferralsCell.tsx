062b8f7059081ece189f9a4bae46faf64df78e69
ice-atlas <121009572+ice-atlas@users.noreply.github.com>
2023-04-27 09:15:21 +0300

    [Team screen]: animated numbers (#50)
    - use animated number value on Team screen

--- src/screens/Team/components/Header/components/Info/components/ReferralsCell.tsx
+++ src/screens/Team/components/Header/components/Info/components/ReferralsCell.tsx
@@ -1,29 +1,36 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import {COLORS} from '@constants/colors';
+import {useAnimatedNumber} from '@hooks/useAnimatedNumber';
 import {userReferralCountSelector} from '@store/modules/Referrals/selectors';
 import {TeamInactiveIcon} from '@svg/TeamInactiveIcon';
 import {t} from '@translations/i18n';
+import {formatNumber} from '@utils/numbers';
 import {font} from '@utils/styles';
-import React from 'react';
+import React, {memo} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {useSelector} from 'react-redux';
 import {rem} from 'rn-units';
 
-export const ReferralsCell = ({color = COLORS.white}: {color?: string}) => {
-  const refsCount = useSelector(userReferralCountSelector);
-  return (
-    <View style={styles.container}>
-      <TeamInactiveIcon color={color} width={rem(38)} height={rem(38)} />
-      <View style={styles.body}>
-        <Text style={[styles.titleText, {color}]}>
-          {t('team.header.referrals')}
-        </Text>
-        <Text style={[styles.valueText, {color}]}>{refsCount}</Text>
+export const ReferralsCell = memo(
+  ({color = COLORS.white}: {color?: string}) => {
+    const refsCount = useSelector(userReferralCountSelector);
+
+    const animatedRefsCount = useAnimatedNumber(refsCount, formatNumber);
+
+    return (
+      <View style={styles.container}>
+        <TeamInactiveIcon color={color} width={rem(38)} height={rem(38)} />
+        <View style={styles.body}>
+          <Text style={[styles.titleText, {color}]}>
+            {t('team.header.referrals')}
+          </Text>
+          <Text style={[styles.valueText, {color}]}>{animatedRefsCount}</Text>
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  },
+);
 
 const styles = StyleSheet.create({
   container: {
