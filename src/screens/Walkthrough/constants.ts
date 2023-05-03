928088eb1d25eecdd1320b3d6161bb0f7d81aa73
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2023-05-03 16:02:45 +0300

    chore: changed animation duration

--- src/screens/Walkthrough/constants.ts
+++ src/screens/Walkthrough/constants.ts
@@ -10,6 +10,6 @@ export const CIRCLE_TO_ELEMENT_OFFSET = rem(10);
 export const MAX_CIRCLE_OFFSCREEN_TOP = CIRCLE_DIAMETER * 0.2;
 export const MAX_CIRCLE_OFFSCREEN_BOTTOM = CIRCLE_DIAMETER * 0.06;
 
-export const ANIMATION_CONFIG = {duration: 500};
+export const ANIMATION_CONFIG = {duration: 250};
 
 export const ANIMATION_DELAY = 300;
