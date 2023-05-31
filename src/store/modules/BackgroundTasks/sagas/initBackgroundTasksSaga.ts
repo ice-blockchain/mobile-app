cc18dbc05a18919753c44e785f4e95eab0ab0446
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-05-31 17:20:29 +0400

    chore: process background task error on ios simulator better way

--- src/store/modules/BackgroundTasks/sagas/initBackgroundTasksSaga.ts
+++ src/store/modules/BackgroundTasks/sagas/initBackgroundTasksSaga.ts
@@ -8,46 +8,45 @@ import {
   BackgroundTask,
 } from '@store/modules/BackgroundTasks/config';
 import BackgroundFetch from 'react-native-background-fetch';
-import {all, call} from 'redux-saga/effects';
+import DeviceInfo from 'react-native-device-info';
+import {all, call, SagaReturnType} from 'redux-saga/effects';
+import {isIOS} from 'rn-units';
 
 export function* initBackgroundTasksSaga() {
-  try {
-    yield call(
-      BackgroundFetch.configure,
-      {
-        minimumFetchInterval: 3.156e7, // don't run the default background fetch
-      },
-      taskId => {
-        switch (taskId) {
-          case BackgroundTask.SyncContacts: {
-            store.dispatch(
-              BackgroundTasksActions.SYNC_CONTACTS_BACKGROUND_TASK.STATE.create(
-                {
-                  finishTask: () => BackgroundFetch.finish(taskId),
-                },
-              ),
-            );
-            break;
-          }
-          default: {
-            logError(new Error(`Unknown background task with id ${taskId}`));
-            BackgroundFetch.finish(taskId);
-          }
-        }
-      },
-      taskId => BackgroundFetch.finish(taskId),
-    );
+  const isEmulator: SagaReturnType<typeof DeviceInfo.isEmulator> = yield call(
+    DeviceInfo.isEmulator,
+  );
 
-    yield all(
-      BACKGROUND_TASKS.map(task => call(BackgroundFetch.scheduleTask, task)),
-    );
-  } catch (error) {
-    const isSimulatorError =
-      typeof error === 'string' &&
-      error.includes('BGTaskSchedulerErrorDomain error 1');
-
-    if (!isSimulatorError) {
-      throw error;
-    }
+  // BackgroundFetch doesn't work on simulators and throws an error
+  if (isEmulator && isIOS) {
+    return;
   }
+
+  yield call(
+    BackgroundFetch.configure,
+    {
+      minimumFetchInterval: 3.156e7, // don't run the default background fetch
+    },
+    taskId => {
+      switch (taskId) {
+        case BackgroundTask.SyncContacts: {
+          store.dispatch(
+            BackgroundTasksActions.SYNC_CONTACTS_BACKGROUND_TASK.STATE.create({
+              finishTask: () => BackgroundFetch.finish(taskId),
+            }),
+          );
+          break;
+        }
+        default: {
+          logError(new Error(`Unknown background task with id ${taskId}`));
+          BackgroundFetch.finish(taskId);
+        }
+      }
+    },
+    taskId => BackgroundFetch.finish(taskId),
+  );
+
+  yield all(
+    BACKGROUND_TASKS.map(task => call(BackgroundFetch.scheduleTask, task)),
+  );
 }
