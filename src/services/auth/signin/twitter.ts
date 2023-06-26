5dbdbf9556a2abe6a00772d0f2bcdac0da82858a
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-06-26 13:25:27 +0400

    feat: twitter firebase (#84)
    Use `Firebase->OAuthProvider` methods for Twitter sign in without the
    need of 3rd party libs

--- src/services/auth/signin/twitter.ts
+++ src/services/auth/signin/twitter.ts
@@ -1,5 +1,31 @@
 // SPDX-License-Identifier: ice License 1.0
 
-export const startTwitterSignIn = () => {
-  throw new Error('Temporary disabled');
+import auth from '@react-native-firebase/auth';
+import {checkProp} from '@utils/guards';
+import {removeInvalidUsernameCharacters} from '@utils/username';
+
+export const twitterSignIn = async () => {
+  try {
+    const userCredential = await auth().signInWithProvider('twitter.com');
+
+    return {
+      cancelled: false,
+      userInfo: {
+        userHandle: removeInvalidUsernameCharacters(
+          userCredential.additionalUserInfo?.username ?? '',
+        ),
+        firstName: userCredential.user.displayName,
+        lastName: null,
+      },
+    } as const;
+  } catch (error) {
+    if (
+      checkProp(error, 'code') &&
+      (error.code === 'auth/web-context-canceled' ||
+        error.code === 'auth/popup-closed-by-user')
+    ) {
+      return {cancelled: true} as const;
+    }
+    throw error;
+  }
 };
