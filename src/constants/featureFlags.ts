24ce842ff8263464f64afb0d1e5a69b8669ee96a
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-06-28 11:05:11 +0400

    chore: enable phone and email link for everyone

--- src/constants/featureFlags.ts
+++ src/constants/featureFlags.ts
@@ -1,16 +1,14 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import {deviceMainLocale} from '@translations/i18n';
-
 /**
  * Is Email Link sign in flow used instead of Email + Password
  */
-export const isEmailLinkSignIn = deviceMainLocale.languageCode !== 'zh';
+export const isEmailLinkSignIn = true;
 
 /**
  * Is sign in with phone number enabled
  */
-export const isPhoneNumberAuthEnabled = deviceMainLocale.languageCode !== 'zh';
+export const isPhoneNumberAuthEnabled = true;
 
 /**
  * Is linking phone number on the Team Screen enabled
