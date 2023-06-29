3eb1086efe32f11271f61eabf3e47ac71acfb69e
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-06-29 16:25:35 +0400

    chore: process token refresh race condition

--- src/api/client/interceptors/response.ts
+++ src/api/client/interceptors/response.ts
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import {refreshAuthToken} from '@services/auth';
+import {AuthToken} from '@services/auth/types';
 import {store} from '@store/configureStore';
 import {AccountActions} from '@store/modules/Account/actions';
 import {authTokenSelector} from '@store/modules/Account/selectors';
 import {AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
+
+let tokenRefreshPromise: Promise<AuthToken | null> | null = null;
 
 function onRejected(instance: AxiosInstance) {
   return async (error: {
@@ -23,7 +26,18 @@ function onRejected(instance: AxiosInstance) {
           const currentToken = authTokenSelector(store.getState());
 
           if (!originalRequest.pliantRequestRetry && currentToken) {
-            const newToken = await refreshAuthToken(currentToken);
+            if (!tokenRefreshPromise) {
+              /**
+               * In case if multiple requests are failed with 401
+               * The first one runs the token refresh
+               * The rest are waiting for a result
+               */
+              tokenRefreshPromise = refreshAuthToken(currentToken);
+            }
+
+            const newToken = await tokenRefreshPromise;
+
+            tokenRefreshPromise = null;
 
             /**
              * token is null if user is already signed out
