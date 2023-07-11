854af86c5c0703b4a9df0b4312fdc863dd55e830
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-07-11 14:58:22 +0400

    fix(keychain): change keychain storage

--- src/services/keychain/index.ts
+++ src/services/keychain/index.ts
@@ -2,14 +2,23 @@
 
 import * as Keychain from 'react-native-keychain';
 
+/**
+ * Using FB storage because with a default one, on some devices
+ * "Could not decrypt data with alias: ..." error is thrown.
+ * https://github.com/oblador/react-native-keychain/issues/567
+ */
 export const setSecureValue = (key: string, value: string) => {
   return Keychain.setGenericPassword(key, value, {
     service: key,
+    storage: Keychain.STORAGE_TYPE.FB,
   });
 };
 
 export const getSecureValue = async (key: string) => {
-  const result = await Keychain.getGenericPassword({service: key});
+  const result = await Keychain.getGenericPassword({
+    service: key,
+    storage: Keychain.STORAGE_TYPE.FB,
+  });
   if (result) {
     return result.password;
   }
