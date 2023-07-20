19cfd35021d0f4113717aeea3adf18ce1adb17ad
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-07-20 18:03:42 +0400

    chore: update react-native to 0.72.3

--- ios/ice/AppDelegate.h
+++ ios/ice/AppDelegate.h
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: ice License 1.0
 
-#import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 #import <UserNotifications/UNUserNotificationCenter.h>
 #import <FirebaseMessaging.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, UNUserNotificationCenterDelegate, FIRMessagingDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
+@interface AppDelegate : RCTAppDelegate
 
 // add this line to the @interface section of AppDelegate
 @property (nonatomic, strong) NSString *moeDeeplink;
