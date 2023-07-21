fbaea4f3187b8d95bd73e6fa56fa75a793c709fe
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-07-21 12:36:21 +0400

    chore: update react-native-config, react-native-reanimated, google-sign-in, fix android build issues

--- src/screens/Modals/QRCodeScanner/index.tsx
+++ src/screens/Modals/QRCodeScanner/index.tsx
@@ -4,8 +4,10 @@ import {COLORS} from '@constants/colors';
 import {commonStyles} from '@constants/styles';
 import {Header} from '@navigation/components/Header';
 import {WelcomeStackParamList} from '@navigation/Welcome';
-import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import {RouteProp, useRoute} from '@react-navigation/native';
 import {useCameraPermissions} from '@screens/Modals/QRCodeScanner/hooks/useCameraPermissions';
+import {BarCodeScanner} from 'expo-barcode-scanner';
+import {Camera, CameraType} from 'expo-camera';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 
@@ -15,7 +17,7 @@ export type QRCodeScannerParams = {
 
 export const QRCodeScanner = () => {
   const route = useRoute<RouteProp<WelcomeStackParamList, 'QRCodeScanner'>>();
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
   const {onDetect} = route.params;
 
   const {permissionsGranted} = useCameraPermissions();
@@ -23,7 +25,19 @@ export const QRCodeScanner = () => {
   return (
     <View style={commonStyles.flexOne}>
       <Header backgroundColor="transparent" color={COLORS.white} />
-      {permissionsGranted ? null : (
+      {permissionsGranted ? (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          type={CameraType.back}
+          barCodeScannerSettings={{
+            barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
+          }}
+          onBarCodeScanned={(...d) => {
+            console.log(d);
+            onDetect('');
+          }}
+        />
+      ) : (
         <View style={[StyleSheet.absoluteFill, styles.stub]} />
       )}
     </View>
