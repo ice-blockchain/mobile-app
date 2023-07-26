e2d65b89fed42353c4b0a7b35eb03c95bdd844d1
ice-hector <96414297+ice-hector@users.noreply.github.com>
2023-07-26 14:35:20 +0300

    chore: add rate dates recording

--- src/store/modules/RateApp/sagas/checkRateAppConditionSaga.ts
+++ src/store/modules/RateApp/sagas/checkRateAppConditionSaga.ts
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import {RateData, User} from '@api/user/types';
 import {dayjs} from '@services/dayjs';
+import {AccountActions} from '@store/modules/Account/actions';
 import {
   firstMiningDateSelector,
   firstRateShowingDateSelector,
   secondRateShowingDateSelector,
   thirdRateShowingDateSelector,
+  unsafeUserSelector,
 } from '@store/modules/Account/selectors';
 import {isAppActiveSelector} from '@store/modules/AppCommon/selectors';
 import {RateAppActions} from '@store/modules/RateApp/actions';
 import {RateAppSelectors} from '@store/modules/RateApp/selectors';
-import {put, SagaReturnType, select} from 'redux-saga/effects';
+import {call, put, SagaReturnType, select} from 'redux-saga/effects';
 
 const FIRST_APPEAR_DAYS = 10;
 const SECOND_APPEAR_DAYS = 35;
@@ -35,12 +38,14 @@ export function* checkRateAppConditionSaga() {
     return;
   }
 
+  const user: User = yield select(unsafeUserSelector);
+
   let isRateAppShown: SagaReturnType<typeof RateAppSelectors.isRateAppShown> =
     yield select(RateAppSelectors.isRateAppShown);
 
   isRateAppShown = yield select(RateAppSelectors.isRateAppShown);
 
-  /** Show if it hasn't been shown before after 10 days of first mining happened  */
+  /** Show if it hasn't been shown before (after 10 days of first mining happened)  */
   const showFirstTime =
     !isRateAppShown &&
     firstMiningDate &&
@@ -63,7 +68,34 @@ export function* checkRateAppConditionSaga() {
     dayjs().diff(firstMiningDate, 'day') >=
       FIRST_APPEAR_DAYS + SECOND_APPEAR_DAYS + THIRD_APPEAR_DAYS;
 
-  if (showFirstTime || showSecondTime || showThirdTime) {
+  if (user && (showFirstTime || showSecondTime || showThirdTime)) {
+    let params: RateData = {};
+    if (user.clientData?.rate) {
+      params = {...user.clientData.rate};
+    }
+
+    if (showFirstTime) {
+      params.firstRateShowingDate = dayjs().toISOString();
+    } else if (showSecondTime) {
+      params.secondRateShowingDate = dayjs().toISOString();
+    } else if (showThirdTime) {
+      params.thirdRateShowingDate = dayjs().toISOString();
+    }
+
+    yield call(updateRateData, user, params);
     yield put(RateAppActions.SHOW_RATE_APP.START.create());
+  }
+}
+
+function* updateRateData(user: User, params?: RateData) {
+  if (params) {
+    yield put(
+      AccountActions.UPDATE_ACCOUNT.START.create({
+        clientData: {
+          ...(user.clientData ?? {}),
+          rate: {...params},
+        },
+      }),
+    );
   }
 }
