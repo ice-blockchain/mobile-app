65709c72a5e6f43641713e6d4f39e379793f6287
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-08-01 10:21:52 +0400

    fix: do not run init modules flow on logout (might lead to init error)

--- src/store/modules/AppCommon/sagas/appInitializedHandler.ts
+++ src/store/modules/AppCommon/sagas/appInitializedHandler.ts
@@ -4,12 +4,23 @@ import {AppCommonActions} from '@store/modules/AppCommon/actions';
 import {
   failedInitializeActionSelector,
   initSuccessSelector,
+  isAppInitializedSelector,
 } from '@store/modules/AppCommon/selectors';
 import {failedReasonSelector} from '@store/modules/UtilityProcessStatuses/selectors';
 import {t} from '@translations/i18n';
 import {call, put, SagaReturnType, select, take} from 'redux-saga/effects';
 
 function* isModulesInitComplete() {
+  const isAppInitialized: ReturnType<typeof isAppInitializedSelector> =
+    yield select(isAppInitializedSelector);
+
+  /**
+   * In case of Logout we don't need to init modules again
+   */
+  if (isAppInitialized) {
+    return true;
+  }
+
   const initSuccess: ReturnType<typeof initSuccessSelector> = yield select(
     initSuccessSelector,
   );
