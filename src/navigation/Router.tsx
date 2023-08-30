7b4809a8444c6dc4a5b8fbbe9d67c0ca0789fa71
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-08-30 16:09:40 +0400

    test: record a video + generate thumbnails

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -1,55 +1,20 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import {AuthNavigator} from '@navigation/Auth';
-import {InitializationError} from '@navigation/components/InitializationError';
 import {useRouteNameChange} from '@navigation/hooks/useRouteNameChange';
 import {MainNavigator} from '@navigation/Main';
 import {theme} from '@navigation/theme';
-import {navigationReadyResolver, navigationRef} from '@navigation/utils';
-import {WelcomeNavigator} from '@navigation/Welcome';
+import {navigationRef} from '@navigation/utils';
 import {NavigationContainer} from '@react-navigation/native';
 import {routingInstrumentation} from '@services/logging';
-import {
-  isRegistrationCompleteSelector,
-  userSelector,
-} from '@store/modules/Account/selectors';
 import {AnalyticsEventLogger} from '@store/modules/Analytics/constants';
 import {useAppStateListener} from '@store/modules/AppCommon/hooks/useAppStateListener';
-import {appInitStateSelector} from '@store/modules/AppCommon/selectors';
 import {useOpenUrlListener} from '@store/modules/Linking/hooks/useOpenUrlListener';
 import {useInitNotifications} from '@store/modules/PushNotifications/hooks/useInitNotifications';
 import {useDispatchRestartWalkthrough} from '@store/modules/Walkthrough/hooks/useDispatchRestartWalkthrough';
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback} from 'react';
 import RNBootSplash from 'react-native-bootsplash';
-import {useSelector} from 'react-redux';
 
 function ActiveNavigator() {
-  const user = useSelector(userSelector);
-  const isRegistrationComplete = useSelector(isRegistrationCompleteSelector);
-  const appInitState = useSelector(appInitStateSelector);
-
-  useEffect(() => {
-    if (appInitState === 'success') {
-      navigationReadyResolver();
-    }
-  }, [appInitState]);
-
-  if (appInitState === 'loading') {
-    return null;
-  }
-
-  if (appInitState === 'error') {
-    return <InitializationError />;
-  }
-
-  if (!user) {
-    return <AuthNavigator />;
-  }
-
-  if (!isRegistrationComplete) {
-    return <WelcomeNavigator />;
-  }
-
   return <MainNavigator />;
 }
 
--- src/screens/HomeFlow/Home/index.tsx
+++ src/screens/HomeFlow/Home/index.tsx
@@ -1,57 +1,132 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import {PullToRefreshContainer} from '@components/PullToRefreshContainer';
+import {PrimaryButton} from '@components/Buttons/PrimaryButton';
 import {COLORS} from '@constants/colors';
-import {commonStyles} from '@constants/styles';
-import {useBottomTabBarOffsetStyle} from '@navigation/hooks/useBottomTabBarOffsetStyle';
-import {useFocusStatusBar} from '@navigation/hooks/useFocusStatusBar';
-import {HomeHeader} from '@screens/HomeFlow/Home/components/Header';
-import {Overview} from '@screens/HomeFlow/Home/components/Overview';
-import {PAGE_HEIGHT, Pager} from '@screens/HomeFlow/Home/components/Pager';
-import {SocialLinks} from '@screens/HomeFlow/Home/components/SocialLinks';
-import {Tasks} from '@screens/HomeFlow/Home/components/Tasks';
-import {Team} from '@screens/HomeFlow/Home/components/Team';
-import {useAchievementsWalkthrough} from '@screens/HomeFlow/Home/hooks/useAchievementsWalkthrough';
-import {useHandleScrollToParam} from '@screens/HomeFlow/Home/hooks/useHandleScrollToParam';
-import {useHomeRefresh} from '@screens/HomeFlow/Home/hooks/useHomeRefresh';
-import React, {memo} from 'react';
-import {StyleSheet, View} from 'react-native';
-import Animated, {useSharedValue} from 'react-native-reanimated';
+import {getErrorMessage} from '@utils/errors';
+import {Camera, CameraType, VideoQuality} from 'expo-camera';
+import React, {memo, useRef, useState} from 'react';
+import {Alert, Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {createThumbnail} from 'react-native-create-thumbnail';
+
+type Video = {uri: string; duration: number};
+type Frame = {uri: string};
 
 export const Home = memo(() => {
-  useFocusStatusBar({style: 'dark-content'});
-  const tabBarOffset = useBottomTabBarOffsetStyle();
-  const {onRefresh, refreshing} = useHomeRefresh();
+  const [permission, requestPermission] = Camera.useCameraPermissions();
 
-  const translateY = useSharedValue(0);
+  const cameraRef = useRef<Camera>(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [video, setVideo] = useState<Video | null>(null);
+  const [frames, setFrames] = useState<Frame[]>([]);
+  const [frameGenerationDuration, setFrameGenerationDuration] = useState<
+    number | null
+  >(null);
 
-  const {animatedScrollViewRef} = useHandleScrollToParam();
-  const {elementRef, onElementLayout} = useAchievementsWalkthrough();
+  const onStartRecord = async () => {
+    try {
+      setIsRecording(true);
+      const start = Date.now();
+      const result = await cameraRef.current?.recordAsync({
+        mute: true,
+        quality: VideoQuality['4:3'],
+      });
+      if (result?.uri) {
+        setVideo({uri: result.uri, duration: Date.now() - start});
+      }
+    } catch (error) {
+      Alert.alert('oops', getErrorMessage(error));
+    } finally {
+      setIsRecording(false);
+    }
+  };
+
+  const onStopRecord = () => {
+    cameraRef.current?.stopRecording();
+  };
+
+  const onGenerateImages = async () => {
+    try {
+      if (video) {
+        const start = Date.now();
+        setIsGenerating(true);
+        setFrameGenerationDuration(null);
+        setFrames([]);
+        const numberOfFrames = Math.floor(video.duration / 333) + 1;
+        const thumbnails = await Promise.all(
+          Array(numberOfFrames)
+            .fill(null)
+            .map((_, index) =>
+              createThumbnail({
+                url: video.uri,
+                timeStamp: index * 333,
+              }),
+            ),
+        );
+        setFrameGenerationDuration(Date.now() - start);
+        setFrames(thumbnails.map(t => ({uri: t.path})));
+      }
+    } catch (error) {
+      Alert.alert('oops', getErrorMessage(error));
+    } finally {
+      setIsGenerating(false);
+    }
+  };
+
+  if (!permission?.granted) {
+    return (
+      <View style={styles.container}>
+        <Text>No Permissions</Text>
+        <PrimaryButton
+          text={'Request Permissions'}
+          onPress={requestPermission}
+        />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <HomeHeader translateY={translateY} transitionOffset={PAGE_HEIGHT} />
-      <PullToRefreshContainer
-        style={styles.container}
-        onScrollTranslateY={translateY}
-        onRefresh={onRefresh}
-        animatedScrollViewRef={animatedScrollViewRef}
-        refreshing={refreshing}>
-        <Animated.ScrollView
-          ref={animatedScrollViewRef}
-          contentContainerStyle={tabBarOffset.current}
-          showsVerticalScrollIndicator={false}>
-          <Pager />
-          <View style={commonStyles.baseSubScreen}>
-            <Overview translateY={translateY} topOffset={PAGE_HEIGHT} />
-            <Team />
-            <View ref={elementRef} onLayout={onElementLayout}>
-              <Tasks />
-            </View>
-            <SocialLinks />
-          </View>
-        </Animated.ScrollView>
-      </PullToRefreshContainer>
+      <Camera
+        ref={cameraRef}
+        style={styles.camera}
+        type={CameraType.back}
+        ratio={'4:3'}
+      />
+      {isRecording ? (
+        <PrimaryButton text={'Stop'} onPress={onStopRecord} />
+      ) : (
+        <PrimaryButton text={'Record'} onPress={onStartRecord} />
+      )}
+      {!!video && (
+        <>
+          <Text>Video Uri: {video.uri}</Text>
+          <Text>Video duration is: {video.duration}ms</Text>
+          <PrimaryButton
+            text={'Generate Images'}
+            onPress={onGenerateImages}
+            disabled={isGenerating}
+            loading={isGenerating}
+          />
+        </>
+      )}
+      {!!frameGenerationDuration && (
+        <>
+          <Text>Frame generation duration is: {frameGenerationDuration}ms</Text>
+          <Text>Number of frames is: {frames.length}</Text>
+        </>
+      )}
+      {frames.length > 0 && (
+        <ScrollView horizontal={true}>
+          {frames.map(frame => (
+            <Image
+              source={{uri: frame.uri}}
+              key={frame.uri}
+              style={styles.frame}
+            />
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
 });
@@ -60,5 +135,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.white,
+  },
+  camera: {
+    width: 300,
+    height: 225,
+    alignSelf: 'center',
+  },
+  frameContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  frame: {
+    width: 200,
+    height: 100,
   },
 });
