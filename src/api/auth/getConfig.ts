47ce6962114288ce0f1335952e975cf559202049
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-10-18 13:12:22 +0400

    chore: add NO_CACHE_HEADERS

--- src/api/auth/getConfig.ts
+++ src/api/auth/getConfig.ts
@@ -2,14 +2,11 @@
 
 import {AuthConfig} from '@api/auth/types';
 import {get} from '@api/client';
+import {NO_CACHE_HEADERS} from '@api/client/getHeaders';
 import {ENV} from '@constants/env';
 
 export function getConfig() {
   return get<AuthConfig>(ENV.AUTH_CONFIG_URL ?? '', undefined, undefined, {
-    headers: {
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      Pragma: 'no-cache',
-      Expires: 0,
-    },
+    headers: NO_CACHE_HEADERS,
   });
 }
