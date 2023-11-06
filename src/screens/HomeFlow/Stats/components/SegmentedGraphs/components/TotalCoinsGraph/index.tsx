fd22df7320197a5d3f20ed4fe1ac689188e259a3
ice-hector <96414297+ice-hector@users.noreply.github.com>
2023-11-06 10:16:20 +0200

    chore: connect filters

--- src/screens/HomeFlow/Stats/components/SegmentedGraphs/components/TotalCoinsGraph/index.tsx
+++ src/screens/HomeFlow/Stats/components/SegmentedGraphs/components/TotalCoinsGraph/index.tsx
@@ -2,6 +2,7 @@
 
 import {TotalCoinsFilter} from '@api/statistics/types';
 import {useGetTotalCoinsBarGraphDataForStatsPeriod} from '@components/BarGraph/hooks/useGetTotalCoinsBarGraphDataForStatsPeriod';
+import {BarGraphData} from '@components/BarGraph/types';
 import {SectionHeader} from '@components/SectionHeader';
 import {AnimatedGraph} from '@screens/HomeFlow/Stats/components/SegmentedGraphs/components/AnimatedGraph';
 import {StatsPeriodSelector} from '@screens/HomeFlow/Stats/components/SegmentedGraphs/components/StatsPeriodSelector';
@@ -18,7 +19,27 @@ export const DEFAULT_FILTER = 'total';
 export const TotalCoinsGraph = memo(() => {
   const [period, setPeriod] = useState<StatsPeriod>(DEFAULT_PERIOD);
   const [filter, setFilter] = useState<TotalCoinsFilter>(DEFAULT_FILTER);
-  const {totalData} = useGetTotalCoinsBarGraphDataForStatsPeriod(period);
+  const {totalData, blockchainData, standardData, preStakingData} =
+    useGetTotalCoinsBarGraphDataForStatsPeriod(period);
+
+  const dataForFilter = useMemo(() => {
+    let data: BarGraphData[] = [];
+    switch (filter) {
+      case 'total':
+        data = totalData;
+        break;
+      case 'on-app':
+        data = standardData;
+        break;
+      case 'on-blockchain':
+        data = blockchainData;
+        break;
+      case 'pre-staked':
+        data = preStakingData;
+        break;
+    }
+    return data;
+  }, [blockchainData, filter, preStakingData, standardData, totalData]);
 
   const ListHeader = useMemo(() => {
     return (
@@ -36,7 +57,7 @@ export const TotalCoinsGraph = memo(() => {
 
   return (
     <AnimatedGraph
-      data={totalData}
+      data={dataForFilter}
       ListHeader={ListHeader}
       type="total_coins"
     />
