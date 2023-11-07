f6588d6532ed89948e4ae44a32163de6c2a2e1e8
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-11-07 11:04:14 +0400

    chore remove web3 lib, use viem instead

--- polyfills.js
+++
@@ -1,8 +0,0 @@
-import 'react-native-get-random-values';
-import 'react-native-url-polyfill/auto';
-import TextEncodingPolyfill from 'text-encoding';
-
-Object.assign(global, {
-  TextEncoder: TextEncodingPolyfill.TextEncoder,
-  TextDecoder: TextEncodingPolyfill.TextDecoder,
-});
--- src/services/ethereum/index.tsx
+++ src/services/ethereum/index.tsx
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import {Web3Context} from 'web3-core/lib/esm/web3_context';
-import {isAddress} from 'web3-validator/lib/esm/validation/address';
+import {Address, createPublicClient, http, isAddress} from 'viem';
+import {mainnet} from 'viem/chains';
 
 export const isValidEthereumAddress = (address: string): boolean => {
-  return true;
   return isAddress(address);
 };
 
 export const isEoaEthereumAddress = async (
   address: string,
 ): Promise<boolean> => {
-  const web3 = new Web3Context('https://mainnet.infura.io');
-  console.log('web3', web3);
-  // const res = await web3.eth.getCode(address);
-  // return res === '0x';
-  return false;
+  const client = createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  });
+
+  const code = await client.getBytecode({address: address as Address});
+  return !code;
 };
