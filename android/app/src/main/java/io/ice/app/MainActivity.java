3b7103fedb31733aa92aee401b53046ec548a24a
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-11-09 12:29:27 +0400

    chore: update react-native-bootsplash, change the splash hiding

--- android/app/src/main/java/io/ice/app/MainActivity.java
+++ android/app/src/main/java/io/ice/app/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    RNBootSplash.init(this); 
+    RNBootSplash.init(this, R.style.BootTheme);
     super.onCreate(null);
     I18nUtil.getInstance().allowRTL(this, false);
   }
