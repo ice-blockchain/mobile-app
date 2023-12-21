f73d3c96e04a96676e7c184b171bf35c8b783022
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-12-21 12:35:19 +0400

    chore: change blockchain address link (#235)

---
+++ src/screens/HomeFlow/BalanceHistory/components/PagerHeader/components/ExternalLink.tsx
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import {Touchable} from '@components/Touchable';
+import {LINKS} from '@constants/links';
+import {SMALL_BUTTON_HIT_SLOP} from '@constants/styles';
+import {unsafeUserSelector} from '@store/modules/Account/selectors';
+import {balanceSummarySelector} from '@store/modules/Tokenomics/selectors';
+import {ArrowLink} from '@svg/ArrowLink';
+import {openLinkWithInAppBrowser} from '@utils/device';
+import React from 'react';
+import {StyleProp, ViewStyle} from 'react-native';
+import {useSelector} from 'react-redux';
+
+type Props = {
+  style?: StyleProp<ViewStyle>;
+};
+
+export const ExternalLink = ({style}: Props) => {
+  const user = useSelector(unsafeUserSelector);
+  const balanceSummary = useSelector(balanceSummarySelector);
+
+  const onLinkPress = () => {
+    if (
+      user.miningBlockchainAccountAddress &&
+      balanceSummary &&
+      balanceSummary.totalMiningBlockchain !== '0.00'
+    ) {
+      openLinkWithInAppBrowser({
+        url: `${LINKS.ETHERSCAN}/${user.miningBlockchainAccountAddress}`,
+      });
+    }
+  };
+
+  return (
+    <Touchable
+      onPress={onLinkPress}
+      style={style}
+      hitSlop={SMALL_BUTTON_HIT_SLOP}>
+      <ArrowLink />
+    </Touchable>
+  );
+};
