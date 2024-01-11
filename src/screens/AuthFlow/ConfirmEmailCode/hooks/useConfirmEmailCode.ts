5d2117638c30638312cc45a970f6426d232ca978
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-01-11 14:38:00 +0200

    fix: issue with code

--- src/screens/AuthFlow/ConfirmEmailCode/hooks/useConfirmEmailCode.ts
+++ src/screens/AuthFlow/ConfirmEmailCode/hooks/useConfirmEmailCode.ts
@@ -16,7 +16,7 @@ import {
   migrationEmailSelector,
   temporaryEmailSelector,
 } from '@store/modules/Validation/selectors';
-import {useCallback, useEffect, useState} from 'react';
+import {useCallback, useEffect} from 'react';
 import {BackHandler} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 
@@ -24,17 +24,14 @@ export const useConfirmEmailCode = () => {
   const route = useRoute<RouteProp<AuthStackParamList, 'MigrationEmailCode'>>();
   const {isPhoneMigrationFlow} = route.params;
 
-  const [email, setEmail] = useState<string | null | undefined>('');
-  const [code, setCode] = useState<string | null | undefined>('');
-
   const dispatch = useDispatch();
 
   const navigation =
     useNavigation<NativeStackNavigationProp<AuthStackParamList>>();
-  const temporaryEmail = useSelector(temporaryEmailSelector, () => true);
-  const temporaryCode = useSelector(emailVerificationCodeSelector, () => true);
-  const migrationEmail = useSelector(migrationEmailSelector, () => true);
-  const migrationCode = useSelector(migrationEmailCodeSelector, () => true);
+  const temporaryEmail = useSelector(temporaryEmailSelector);
+  const temporaryCode = useSelector(emailVerificationCodeSelector);
+  const migrationEmail = useSelector(migrationEmailSelector);
+  const migrationCode = useSelector(migrationEmailCodeSelector);
 
   const validateError = useSelector(
     failedReasonSelector.bind(null, AccountActions.SIGN_IN_EMAIL_CODE),
@@ -58,22 +55,6 @@ export const useConfirmEmailCode = () => {
   );
 
   useEffect(() => {
-    if (isPhoneMigrationFlow) {
-      setEmail(migrationEmail);
-      setCode(migrationCode);
-    } else {
-      setEmail(temporaryEmail);
-      setCode(temporaryCode);
-    }
-  }, [
-    temporaryEmail,
-    temporaryCode,
-    migrationEmail,
-    migrationCode,
-    isPhoneMigrationFlow,
-  ]);
-
-  useEffect(() => {
     console.log('isMigrationFlow: ', isPhoneMigrationFlow);
     if (isPhoneMigrationFlow) {
       dispatch(AccountActions.MIGRATE_EMAIL_WITH_CODE.START.create());
@@ -87,8 +68,8 @@ export const useConfirmEmailCode = () => {
   }, [validateError, navigation, isPhoneMigrationFlow]);
 
   return {
-    email,
-    code,
+    email: isPhoneMigrationFlow ? migrationEmail : temporaryEmail,
+    code: isPhoneMigrationFlow ? migrationCode : temporaryCode,
     validateError,
     goBack,
   };
