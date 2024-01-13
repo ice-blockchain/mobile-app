7e46af8dc26a2a36807567fc62a83af4aecbb4dd
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2024-01-13 13:17:21 +0300

    chore: more light app design changes

--- src/store/modules/AppUpdate/sagas/index.ts
+++ src/store/modules/AppUpdate/sagas/index.ts
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import {isLightDesign} from '@constants/featureFlags';
 import {AppCommonActions} from '@store/modules/AppCommon/actions';
 import {checkAppUpdatedSaga} from '@store/modules/AppUpdate/sagas/checkAppUpdatedSaga';
 import {updateRequiredSaga} from '@store/modules/AppUpdate/sagas/updateRequiredSaga';
 import {DeviceActions} from '@store/modules/Devices/actions';
 import {takeLatest} from 'redux-saga/effects';
 
-export const appUpdateWatchers = [
-  takeLatest(
-    DeviceActions.UPDATE_DEVICE_METADATA.FAILED.type,
-    updateRequiredSaga,
-  ),
-  takeLatest(
-    AppCommonActions.UPDATE_SPLASH_VISIBLE_STATE.HIDE.type,
-    checkAppUpdatedSaga,
-  ),
-];
+export const appUpdateWatchers = isLightDesign
+  ? []
+  : [
+      takeLatest(
+        DeviceActions.UPDATE_DEVICE_METADATA.FAILED.type,
+        updateRequiredSaga,
+      ),
+      takeLatest(
+        AppCommonActions.UPDATE_SPLASH_VISIBLE_STATE.HIDE.type,
+        checkAppUpdatedSaga,
+      ),
+    ];
