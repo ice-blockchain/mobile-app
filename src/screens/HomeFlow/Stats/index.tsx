71f2a3a2bf2fb176b7e23c1c7965985f8dd92f9e
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2024-01-16 11:04:25 +0300

    fix: stats animations

--- src/screens/HomeFlow/Stats/index.tsx
+++ src/screens/HomeFlow/Stats/index.tsx
@@ -5,7 +5,7 @@ import {COLORS} from '@constants/colors';
 import {commonStyles} from '@constants/styles';
 import {Header} from '@navigation/components/Header';
 import {useFocusStatusBar} from '@navigation/hooks/useFocusStatusBar';
-import {useNavigation} from '@react-navigation/native';
+import {useIsTabNavigation} from '@navigation/hooks/useIsTabNavigation';
 import {SegmentedGraphs} from '@screens/HomeFlow/Stats/components/SegmentedGraphs';
 import {Summary} from '@screens/HomeFlow/Stats/components/Summary';
 import {t} from '@translations/i18n';
@@ -14,9 +14,7 @@ import {StyleSheet, View} from 'react-native';
 
 export const Stats = memo(() => {
   useFocusStatusBar({style: 'light-content'});
-  const navigation = useNavigation();
-  const showBackButton = navigation.getState()?.type !== 'tab';
-
+  const isTabNavigation = useIsTabNavigation();
   return (
     <View style={styles.container}>
       <LinesBackground />
@@ -24,7 +22,7 @@ export const Stats = memo(() => {
         color={COLORS.white}
         title={t('stats.header_title')}
         backgroundColor={'transparent'}
-        showBackButton={showBackButton}
+        showBackButton={!isTabNavigation}
       />
       <Summary />
       <View style={commonStyles.baseSubScreen}>
