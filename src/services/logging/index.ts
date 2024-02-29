d9a771118736cfcfb8aa80126380a2b23dbe81aa
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-02-29 17:38:06 +0400

    chore(cleanup): remove sentry, update pods

--- src/services/logging/index.ts
+++ src/services/logging/index.ts
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import {ENV} from '@constants/env';
-import {AnyAction} from '@reduxjs/toolkit';
-import * as Sentry from '@sentry/react-native';
 import {getErrorMessage} from '@utils/errors';
 import {checkProp} from '@utils/guards';
-import * as React from 'react';
 import {LogBox} from 'react-native';
 
 /**
@@ -16,35 +12,6 @@ import {LogBox} from 'react-native';
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
 ]);
-
-export const routingInstrumentation =
-  new Sentry.ReactNavigationInstrumentation();
-
-Sentry.init({
-  dsn: ENV.SENTRY_KEY,
-  enabled: !__DEV__,
-  integrations: [
-    new Sentry.ReactNativeTracing({
-      routingInstrumentation,
-    }),
-  ],
-  tracesSampleRate: 0.2,
-});
-
-export const loggingReduxEnhancer = Sentry.createReduxEnhancer({
-  stateTransformer: () => null,
-  actionTransformer: (action: AnyAction) => {
-    // omit payload cuz it can contain private data
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const {payload, ...strippedAction} = action;
-    return strippedAction;
-  },
-});
-
-export function LoggingWrapper(app: React.ComponentType) {
-  return Sentry.wrap(app);
-}
-
 export function logError(error: unknown, extra: Record<string, unknown> = {}) {
   if (__DEV__) {
     console.error(
@@ -55,10 +22,5 @@ export function logError(error: unknown, extra: Record<string, unknown> = {}) {
       checkProp(error, 'stack') && error.stack,
       extra,
     );
-  } else {
-    Sentry.withScope(function (scope) {
-      scope.setUser(null);
-      Sentry.captureException(error, {extra});
-    });
   }
 }
