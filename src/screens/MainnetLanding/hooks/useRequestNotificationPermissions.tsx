aa11250eabc2e94dedd4cf53a6b37f9ac5ed5201
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-03-07 18:35:20 +0400

    fix: ask permissions only when the splash is finished

--- src/screens/MainnetLanding/hooks/useRequestNotificationPermissions.tsx
+++ src/screens/MainnetLanding/hooks/useRequestNotificationPermissions.tsx
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import {isSplashHiddenSelector} from '@store/modules/AppCommon/selectors';
 import {PermissionsActions} from '@store/modules/Permissions/actions';
 import {canAskPermissionSelector} from '@store/modules/Permissions/selectors';
 import {useEffect} from 'react';
@@ -7,15 +8,16 @@ import {useDispatch, useSelector} from 'react-redux';
 
 export function useRequestNotificationPermissions() {
   const dispatch = useDispatch();
+  const isSplashHidden = useSelector(isSplashHiddenSelector);
   const canAskNotificationPermission = useSelector(
     canAskPermissionSelector('pushNotifications'),
   );
 
   useEffect(() => {
-    if (canAskNotificationPermission) {
+    if (canAskNotificationPermission && isSplashHidden) {
       dispatch(
         PermissionsActions.GET_PERMISSIONS.START.create('pushNotifications'),
       );
     }
-  }, [canAskNotificationPermission, dispatch]);
+  }, [canAskNotificationPermission, dispatch, isSplashHidden]);
 }
